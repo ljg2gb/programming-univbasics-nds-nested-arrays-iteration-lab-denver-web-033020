ef1c1d97819e2319cb66305f6cbeb1f99260fd55
Lydia Gregory <ljg2gb@virginia.edu>
2020-03-11 23:15:37 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,4 +1,4 @@
-def join_ingredients(src)
+
   # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
   #
   # Build a new Array that contains strings where each pair of foods is
@@ -6,12 +6,42 @@ def join_ingredients(src)
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
+def join_ingredients(src)
+  
+  outer_results = []
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
+    inner_results = []
+    while element_index < src[row_index].count do
+      inner_results << src[row_index][element_index]
+      element_index += 1
+    end
+    outer_results << ("I love #{inner_results[0]} and #{inner_results[1]} on my pizza")
+    row_index += 1
+  end
+  outer_results
 end
 
-def find_greater_pair(src)
+
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
+def find_greater_pair(src)
+  row_index = 0
+  larger_number = []
+  while row_index < src.count do
+    element_index = 0
+    inner_results = []
+    while element_index < src[row_index].count do
+      sorted = src[row_index].sort
+      inner_results << src[row_index][element_index]
+      element_index += 1
+    end
+    larger_number << sorted[-1]
+    row_index += 1
+  end
+  larger_number
 end
 
 def total_even_pairs(src)
